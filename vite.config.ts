c7d2103c882627f2983af890b16af15f9ad22bac
nissasyb <akuninn52@gmail.com>
2025-02-23 17:11:39 +0700

    Configure GitHub Pages deployment with vite-plugin-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages({
+    branch: "docs",
+    message: "Cyberia is Alive",
+    /** @type {options: GhPagesOptions & { outDir: string } => void} */
+    onPublish: (options) => {
+      console.log(`🎉 Successfully deployed to ${options.branch}!`);
+    },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -14,4 +22,5 @@ export default defineConfig({
     outDir: "build",
     emptyOutDir: true,
   },
+  base: "/cyberia-test/",
 });
